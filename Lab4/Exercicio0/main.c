a199c220e4c8fa1fd88efcf395426f3a1e079e84
Matheus Vinicius <matheus.resende@casadosventos.com.br>
2022-07-08 17:05:59 -0300

    teste

--- Lab4/Exercicio0/main.c
+++ Lab4/Exercicio0/main.c
@@ -9,10 +9,10 @@
 #define LCD_RS  25               //Register select pin
 #define LCD_E   24               //Enable Pin
 
-#define LCD_D4  23               //Data pin D4
-#define LCD_D5  22               //Data pin D5
-#define LCD_D6  21               //Data pin D6
-#define LCD_D7  12               //Data pin D7
+#define LCD_D4  6               //Data pin D4
+#define LCD_D5  13               //Data pin D5
+#define LCD_D6  19               //Data pin D6
+#define LCD_D7  26               //Data pin D7
 
 int main() {
 
@@ -22,13 +22,13 @@ int main() {
 
     int lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E,LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0 ,0);
 
+    lcdClear(lcd);
+    lcdPosition(lcd,  0,0);
     lcdPuts(lcd,"TESTANDO LCD");
-    // lcdClear(lcd);
-    // lcdPosition(lcd,  0,0);
-    // lcdPosition(lcd,0,1);
-    // lcdPuts(lcd,"dale dale dale");
-    // lcdPosition(lcd,9,1);
-    // lcdPutchar(lcd, '0');
+    lcdPosition(lcd,0,1);
+    lcdPuts(lcd,"dale dale dale");
+    lcdPosition(lcd,9,1);
+    lcdPutchar(lcd, '0');
     
     return 0;
 }
